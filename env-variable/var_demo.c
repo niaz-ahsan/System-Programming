2a2f2d3e9ded2d7ab03479930e8e861747e3e35c
Niaz <thatisniaz@gmail.com>
2023-06-11 07:27:03 -0700

    Get Env var in env-variable/

---
+++ env-variable/var_demo.c
@@ -0,0 +1,70 @@
+/*
+user has 3 options g/s/r sent via cmd line arg
+-g - get env var followed by the name(s) of the var
+-s - set env var followed by "name value" format
+-r - removes env var followed by name(s) of the var
+*/
+
+#include <stdio.h>
+#include <stdlib.h> // for getenv(), setenv(), unsetenv() etc.
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int g_set = 0;
+    int s_set = 0;
+    int r_set = 0;
+    int c;
+    while( (c = getopt(argc, argv, "gsr")) != -1 ) {
+        switch(c) {
+            case 'g':
+                g_set = 1;
+                break;
+            case 's':
+                s_set = 1;
+                break;
+            case 'r':
+                r_set = 1;
+                break;
+            case '?':
+                printf("Usage:[exe] -g ENV_VAR_NAME\n");
+                printf("Usage:[exe] -s ENV_VAR_NAME value\n");
+                printf("Usage:[exe] -r ENV_VAR_NAME\n");
+                printf("Multiple ENV_VAR are accepted!\n");
+                exit(1);
+                break;
+        }
+    }
+
+    int options = g_set + s_set + r_set;
+    if (options == 0) {
+        printf("No option provided\n");
+        printf("Usage:[exe] -g ENV_VAR_NAME\n");
+        printf("Usage:[exe] -s ENV_VAR_NAME value\n");
+        printf("Usage:[exe] -r ENV_VAR_NAME\n");
+        printf("Multiple ENV_VAR are accepted!\n");
+        exit(1);
+    } else if (options > 1) {
+        printf("Only one option from below should be provided\n");
+        printf("Usage:[exe] -g ENV_VAR_NAME\n");
+        printf("Usage:[exe] -s ENV_VAR_NAME value\n");
+        printf("Usage:[exe] -r ENV_VAR_NAME\n");
+        printf("Multiple ENV_VAR are accepted!\n");
+        exit(1);
+    }
+
+    if (g_set) {
+        for (int i = optind; i<argc; i++) {
+            char *value = getenv(argv[i]);
+            if(value == NULL) {
+                printf("%s : No Env var found\n", argv[i]);
+            } else {
+                printf("%s : %s\n", argv[i], value);
+            }
+        }
+    } else if (s_set) {
+        printf("Set\n");
+    } else if (r_set) {
+        printf("Remove\n");
+    }
+    return 0;
+}
