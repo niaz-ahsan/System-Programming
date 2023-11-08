794c00da55e206c8e618ec4863966a3515f6f2e8
Niaz Ahsan <thatisniaz@gmail.com>
2023-11-07 20:13:45 -0800

    line,space, tab counts are added

--- ##from_book##/char_count.c
+++ ##from_book##/char_count.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int count = 0;
+    int ch_count = 0;
+    int line_count = 0;
+    int tab_count = 0;
+    int space_count = 0;
     int ch;
     while ((ch = getchar()) != EOF) {
-        if (ch != '\n')
-            count++;
+        if (ch == '\n')
+            line_count++;
+        else if (ch == '\t') 
+            tab_count++;
+        else if (ch == ' ')
+            space_count++;
+        else
+            ch_count++;
     }
-    printf("Characters entered: %d\n", count);
+    printf("Characters entered: %d\nLines entered: %d\nSpaces entered: %d\nTabs entered: %d\n", 
+        ch_count, line_count, space_count, tab_count);
     return 0;
 }
