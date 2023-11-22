d7aeb18cc2edac215f123fbc1a1539984d6cdf04
Niaz Ahsan <thatisniaz@gmail.com>
2023-11-22 08:33:43 -0800

    omit multiple spaces with a single one

--- ##from_book##/char_print.c
+++ ##from_book##/char_print.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int ch; // need int as EOF is an int
+    int ch; // need int as EOF is an int(symbolic const), EOF = -1
+    int prev_ch = -1;
     while((ch = getchar()) != EOF) {
+        if (ch == ' ' && ch == prev_ch) { // ommitting multiple whitespaces with a single one
+            continue;
+        }
+        prev_ch = ch;
         putchar(ch);
     }
     return 0;
