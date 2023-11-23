770ec10fc81438dbe29bd7126025871de1d80804
Niaz Ahsan <thatisniaz@gmail.com>
2023-11-23 14:42:11 -0800

    Variable sizes are checked via sizeof()

---
+++ ##from_book##/2-Types/variable_size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("Size of short: %d bytes\n", (int)sizeof(short));
+	printf("Size of int: %d bytes\n", (int)sizeof(int));
+	printf("Size of long: %d bytes\n", (int)sizeof(long));
+	printf("Size of float: %d bytes\n", (int)sizeof(float));
+	printf("Size of double: %d bytes\n", (int)sizeof(double));
+	printf("Size of char: %d bytes\n", (int)sizeof(char));
+	return 0;
+}
